b9299cb1af7a43ba71908fceb242c72d8fbb1d7f
Zhangzechina <43235208+Zhangzechina@users.noreply.github.com>
2018-10-16 23:39:13 +0800

    chapter2demo

---
+++ chapter2/example2_14_void类型指针用法示例.cpp
@@ -0,0 +1,20 @@
+﻿/*---------------------------------------------------------*
+//example2_14.cpp：void类型指针用法示例
+*---------------------------------------------------------*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     void *vp;
+     char c='A';
+     short int x=97;
+     vp=&c;//char类型指针可直接赋值给void型指针
+     cout<<*(char*)vp<<" "<<*(short int*)vp<<" "<<endl;//显式类型转换控制输出
+     vp=&x;//short类型指针可直接赋值给void型指针
+     cout<<*(short int*)vp<<" "<<*(char*)vp<<" "<<endl;
+     return 0;
+}
+
